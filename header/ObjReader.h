7c8422778c2ced27349d74f43a8cb8c8a8b4d2b4
Shin HyunSeok <shinhyunseok03@gmail.com>
2022-07-12 01:29:08 +0900

    Read vertex from obj dll project

--- header/ObjReader.h
+++ header/ObjReader.h
@@ -1,9 +1,10 @@
 #pragma once
 
-#if defined(OBJ_READER_EXPORT)
+#if defined OBJ_READER_EXPORT
 #define OBJ_READ_API __declspec(dllexport)
 #else
 #define OBJ_READ_API __declspec(dllimport)
 #endif
 
-extern OBJ_READ_API float* __cdecl GetVertexFromObjFileMalloc(const char* objFile);
+extern OBJ_READ_API size_t __cdecl GetVertexLength(FILE* objFile);
+extern OBJ_READ_API float* __cdecl GetVertexFromObjFileMalloc(const char* objFile, size_t* length);
