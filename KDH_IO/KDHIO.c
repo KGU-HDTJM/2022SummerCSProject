e7609dd88dd195a20a0e0f277344205beaf30c38
Shin HyunSeok <shinhyunseok03@gmail.com>
2022-07-29 01:43:34 +0900

    function complete
    마무리작업 필요, HDTJM 코딩 표준에 맞춰서 수정 바람

--- KDH_IO/KDHIO.c
+++ KDH_IO/KDHIO.c
@@ -1,70 +1,145 @@
+#pragma warning (disable : 6031)
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-#define GET_VERTEX_LENGTH_BUFFER_SIZE 0x80
+#include "HDTJMType.h"
+
+#define BUFFER_SIZE 0x80
+#define GetRGBA(red, green, blue, alpha)\
+	(RGBA_t){ (byte_t)red, (byte_t)green, (byte_t)blue, (byte_t)alpha };
+
+typedef union
+{
+	struct
+	{
+		byte_t Red;
+		byte_t Green;
+		byte_t Blue;
+		byte_t Alpha;
+	};
+	int rgba;
+} RGBA_t;
+
+char buf[BUFFER_SIZE] = { 0, };
+
+void StoreVertex(
+	Vector3f_t* outVertex, FILE* fp, int* colorIndex,
+	RGBA_t* colorArr, int* vertexIndexArr, int elementVertex
+)
+{
+	float x = 0;
+	float y = 0;
+	float z = 0;
+	int tempR = 0;
+	int tempG = 0;
+	int tempB = 0;
+	RGBA_t tempColor = { 0, };
+	for (size_t i = 0, j = 0, k = 0; i < elementVertex; i++)
+	{
+		fgets(buf, BUFFER_SIZE, fp);
+		sscanf(buf, "%f %f %f %d %d %d", &x, &y, &z, &tempR, &tempG, &tempB);
+		/*	tempColor.Red = tempR;
+		tempColor.Green = tempG;
+		tempColor.Blue = tempB;*/
+		tempColor = GetRGBA(tempR, tempG, tempB, 0);
+
+		// CMP_COLOR: // color_count == voxel_count; 
+		for (j = 0; j < *colorIndex; j++) // 2(1)
+		{
+			if (colorArr[j].rgba == tempColor.rgba)
+			{
+				goto CMP_ELEMENTS;
+			}
+		}
+		colorArr[*colorIndex].rgba = tempColor.rgba;
+		(* colorIndex)++; // 1(0 -> 1), 
+	CMP_ELEMENTS:
+		for (k = 0; k < vertexIndexArr[j]; k++) // 0
+		{
+			if (outVertex[8 * j + k].X == x 
+				&& outVertex[8 * j + k].Y == y 
+				&& outVertex[8 * j + k].Z == z )
+			{
+				goto ELEMENT_EXIST;
+			}
+		}		
+		outVertex[8 * j + k].X = x;
+		outVertex[8 * j + k].Y = y;
+		outVertex[8 * j + k].Z = z;
+		vertexIndexArr[j]++; // 1(0->1);
+	ELEMENT_EXIST:;
+	}
+}
 
 int main(void)
 {
-	FILE* fp = fopen("C:/Users/dahyu/source/repos/HDJM/resource/testio.obj", "r");//파일 오픈
-	char buf[GET_VERTEX_LENGTH_BUFFER_SIZE] = { 0, };
+	int ret = 0;
+
+	int lineCounter = 0;
 	int element_vertex = 0; //반복할 횟수 저장할 곳 == aaa파일에서 72
+	int colorIndex = 0;
+	Vector3f_t* xyz = NULL;
+	RGBA_t* colorArr = NULL;
+	int* vertexIndex = NULL;
+	RGBA_t tempColor = { 0, };
+
 	char element[9];
 	char vertex[7];
 
-	float xyz[18][4][3] = { 0 };
 
-	int i = 0;
 
-	float x = 0, y = 0, z = 0; //파일 불렀을 때 x,y,z값
-	int red = 0, green = 0, blue = 0; //파일 불렀을 때 rgb값
-
-	int element_vertex_count = 0; //4째줄 찾기 위한 거
-
-	if (!fp) //파일없으면 에러처리
+	FILE* fp = fopen("../resource/testio.ply", "r"); //파일 오픈
+	if (fp == NULL) //파일없으면 에러처리
 	{
 		printf("파일 없다.\n");
 		return -1;
 	}
 
-
-	while (fgets(buf, GET_VERTEX_LENGTH_BUFFER_SIZE, fp) != NULL)
+	for (lineCounter = 0; lineCounter < 4; lineCounter++)
 	{
-		element_vertex_count++;
-		if (element_vertex_count == 3)
+		if (NULL == fgets(buf, BUFFER_SIZE, fp))
 		{
-			fscanf(fp, "%s %s %d\n", element, vertex, &element_vertex);
-			printf("%s", buf); //불러온거 확인용
-			printf("%d", element_vertex);
+			return -1;
 		}
-
-		if (element_vertex_count == 12) //end_header다음줄 가져오기 위해서
-		{
-			printf("%s", buf);
-			fgets(buf, GET_VERTEX_LENGTH_BUFFER_SIZE, fp);
-			for (i = 0; i < element_vertex; i++)
-			{
-				fscanf(fp, "%f %f %f %d %d %d\n", &x, &y, &z, &red, &green, &blue);
-				for (int j = 0; j < 4; j++)
-				{
-					xyz[i][j][0] = x;
-					xyz[i][j][1] = y;
-					xyz[i][j][2] = z;
-				}
-			}
-			for (i = 0; i < element_vertex; i++)
-			{
-				for (int j = 0; j < 4; j++)
-				{
-					printf("%f %f %f\n", xyz[i][j][0], xyz[i][j][1], xyz[i][j][2]);
-				}
-			}
-		}
-
 	}
+
+	sscanf(buf, "%s %s %d", element, vertex, &element_vertex);
+	printf("%s\n", buf); //불러온거 확인용
+	printf("%d\n", element_vertex);
+
+	xyz = malloc(element_vertex * sizeof(Vector3f_t));
+	colorArr = calloc(element_vertex / 8, sizeof(RGBA_t));
+	vertexIndex = calloc(element_vertex / 8, sizeof(int));
+
+	for (; lineCounter < 13; lineCounter++)
+	{
+		if (NULL == fgets(buf, BUFFER_SIZE, fp))
+		{
+			return -1;
+			goto PROC_END;
+		}
+	}
+	printf("%s\n", buf);
+	StoreVertex(xyz, fp, &colorIndex, colorArr, vertexIndex, element_vertex);
+
+	// --- file in end ---
+	int vertexCount = 0;
+	
+	for (size_t i = 0; i < colorIndex * 8; i++)
+	{
+		printf("%f %f %f\t", xyz[i].X, xyz[i].Y, xyz[i].Z);
+		vertexCount++;
+	}
+	printf("\nVertex count : % d\nVoxel count : % d\n", vertexCount, colorIndex);
+
+PROC_END:
+	free(vertexIndex);
+	free(colorArr);
+	free(xyz);
+
 	fclose(fp);//파일 닫기
 
-
-	return 0;
+	return ret;
 }
