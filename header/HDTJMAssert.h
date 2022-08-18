2c4a35fd844661ef02b9086e473c42307335b2d5
Shin HyunSeok <shinhyunseok03@gmail.com>
2022-08-18 22:50:56 +0900

    change headerName

--- header/HDTJMAssert.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include <assert.h>
-
-#define Assert(condition, message) assert(condition && message)
---
+++ header/HDTJMError.h
@@ -0,0 +1,9 @@
+#pragma once
+#include <assert.h>
+
+#if !defined(HDTJMERROR_H)
+#define HDTJMERROR_H
+
+#define Assert(condition, message) assert(condition && message)
+
+#endif
