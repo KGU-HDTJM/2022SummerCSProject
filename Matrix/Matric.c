5cb1ae84c23dadaf546429e9e6c564af48d19296
mjkim <67480149+m98541@users.noreply.github.com>
2022-08-24 18:53:26 +0900

    matrix

--- Matrix/Matric.c
+++ Matrix/Matric.c
@@ -6,7 +6,7 @@
 #define AXISY 2
 #define AXISZ 3
 
-void Trans(Vector4f_t transVector)
+Matrix4_t Trans(Vector4f_t transVector)
 {
 	Matrix4_t mat;
 	mat._11 = 1; mat._12 = 0; mat._13 = 0; mat._14 = transVector.X;
@@ -14,10 +14,12 @@ void Trans(Vector4f_t transVector)
 	mat._31 = 0; mat._32 = 0; mat._33 = 1; mat._34 = transVector.Z;
 	mat._41 = 0; mat._42 = 0; mat._43 = 0; mat._44 = 1;
 
+	return mat;
+
 }
 
 
-void Rot(Vector4f_t transVector,int axis,int angle)
+Matrix4_t Rot(Vector4f_t transVector,int axis,int angle)
 {	
 	Matrix4_t mat;
 	if (axis == AXISX) 
@@ -41,20 +43,24 @@ void Rot(Vector4f_t transVector,int axis,int angle)
 		mat._31 = 0;		 mat._32 = 0;		   mat._33 = 0;			  mat._34 = 1;
 		mat._41 = 0;		 mat._42 = 0;		   mat._43 = 0;			  mat._44 = 1;
 	}
-	
+	return mat;
 }
 
 
-void Scale(float Scale)
+Matrix4_t Scale(float Scale)
 {
 	Matrix4_t mat;
 	mat._11 = Scale;	mat._12 = 0;		   mat._13 = 0;		    mat._14 = 0;
 	mat._21 = 0;		mat._22 = Scale;	   mat._23 = 0;			mat._24 = 0;
 	mat._31 = 0;		mat._32 = 0;		   mat._33 = Scale;		mat._34 = 0;
 	mat._41 = 0;		mat._42 = 0;		   mat._43 = 0;			mat._44 = 1;
+
+	return mat;
 }
 
-void Nomalization(Vector4f_t vector)
+
+
+Vector4f_t  Nomalization(Vector4f_t vector)
 {
 	float vectorSize = sqrt(pow(vector.X, 2)
 							+ pow(vector.Y, 2)
@@ -65,7 +71,20 @@ void Nomalization(Vector4f_t vector)
 	nomalVector.X = vector.X / vectorSize;
 	nomalVector.Y = vector.Y / vectorSize;
 	nomalVector.Z = vector.Z / vectorSize;
+	nomalVector.W = 1;
+	return nomalVector;
 
 }
 
-void Projection(,float w, float h)
+
+Matrix4_t ProJectionFrustum(float left, float right,float bottom ,float top ,float near,float far)
+{
+	Matrix4_t mat;
+	mat._11 = (2*near)/(right - left);		mat._12 = 0;		   mat._13 = (right+left)/(right - left);mat._14 = 0;
+	mat._21 = 0;							mat._22 = (2 * near) / (top - bottom);		mat._23 = (top + bottom)/(top - bottom); mat._24 = 0;
+	mat._31 = 0;							mat._32 = 0;		    mat._33 = -(far + near) / (far - near);  mat._34 = -2 * (near + far) / (far - near);
+	mat._41 = 0;							mat._42 = 0;		   mat._43 = -1;			  mat._44 = 0;
+	return mat;
+}
+
+
